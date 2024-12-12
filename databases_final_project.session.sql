67651a93d657af7c177c7db711208c72a1a2f90a
enochappiah <eappiah4@jhu.edu>
2024-12-12 00:06:11 -0500

    recomitting data files and scaffolfing to input data from datasets

--- databases_final_project.session.sql
+++ databases_final_project.session.sql
@@ -1,2 +1,25 @@
 use soccer_db;
-drop TABLE IF EXISTS session;
+
+CREATE TABLE IF NOT EXISTS TEAM (
+    team_id INT PRIMARY KEY,
+    team_name VARCHAR(40) NOT NULL,
+    team_city VARCHAR(40) NOT NULL,
+    team_country VARCHAR(40) NOT NULL -- Added team_country
+)
+
+CREATE TABLE IF NOT EXISTS LEAGUE (
+    league_id INT PRIMARY KEY,
+    league_name VARCHAR(40) NOT NULL,
+    league_country VARCHAR(40) NOT NULL
+)
+
+-- CREATE TABLE IF NOT EXISTS WINS (
+--     team_id INT,
+--     league_id INT,
+--     wins INT,
+--     PRIMARY KEY (team_id, league_id),
+--     FOREIGN KEY (team_id) REFERENCES TEAM(team_id),
+--     FOREIGN KEY (league_id) REFERENCES LEAGUE(league_id)
+-- )
+
+
---
+++ population.py
@@ -0,0 +1,6 @@
+import pandas as pd  
+import sqlite3
+import numpy as np 
+import mysql.connector as sql
+import os
+
